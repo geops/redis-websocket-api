aa953557948f848d956a4a1152c7f488649f70c6
Milan Oberkirch | geOps <milan.oberkirch@geops.de>
2019-09-26 15:40:32 +0200

    Bump version for PyPi release

--- setup.py
+++ setup.py
@@ -5,28 +5,27 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md')) as f:
+with open(os.path.join(here, "README.md")) as f:
     README = f.read()
-with open(os.path.join(here, 'requirements.txt')) as f:
-    requires = [line.strip() for line in f if len(line) > 1 and not line.startswith('#')]
+with open(os.path.join(here, "requirements.txt")) as f:
+    requires = [
+        line.strip() for line in f if len(line) > 1 and not line.startswith("#")
+    ]
 
 setup(
-    name='redis-websocket-api',
-    version='0.0.2-2',
-    description='Redis-over-WebSocket API on top of websockets and aioredis',
+    name="redis-websocket-api",
+    version="0.0.3",
+    description="Redis-over-WebSocket API on top of websockets and aioredis",
     long_description=README,
-    long_description_content_type='text/markdown',
-    url='https://github.com/geops/redis-websocket-api',
-    author='Milan Oberkirch | geOps',
-    author_email='milan.oberkirch@geops.de',
-    license='MIT',
-    keywords='tralis websocket websockets aioredis redis',
-    packages=['redis_websocket_api'],
-    extras_require={
-        'testing': ['pytest'],
-        'geo': ['pyproj']
-    },
-    python_requires='>=3.5',
+    long_description_content_type="text/markdown",
+    url="https://github.com/geops/redis-websocket-api",
+    author="Milan Oberkirch | geOps",
+    author_email="milan.oberkirch@geops.de",
+    license="MIT",
+    keywords="tralis websocket websockets aioredis redis",
+    packages=["redis_websocket_api"],
+    extras_require={"testing": ["pytest"], "geo": ["pyproj"]},
+    python_requires=">=3.5",
     install_requires=requires,
     classifiers=[
         "Programming Language :: Python :: 3",
